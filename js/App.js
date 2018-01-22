2ff3cded883e8203b6b028f118624de51571283b
Michał Świtoń <michal.switon@op.pl>
2018-01-22 02:48:20 +0100

    new kanban app

---
+++ js/App.js
@@ -0,0 +1,43 @@
+// KOMUNIKACJA Z SERWEREM
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': '2810',
+  'X-Auth-Token': 'bc22c0328f12cd71fd32ef250332299f'
+};
+
+$.ajaxSetup({
+	headers: myHeaders
+});
+
+$.ajax({
+    url: baseUrl + '/board',
+    method: 'GET',
+    success: function(response) {
+      setupColumns(response.columns);
+    }
+});
+
+// TWORZENIE KOLUMN
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+  		var col = new Column(column.id, column.name);
+        board.createColumn(col);
+    });
+}
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+  		var col = new Column(column.id, column.name);
+        board.createColumn(col);
+        setupCards(col, column.cards);
+    });
+}
+
+// TWORZENIE KART
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+        var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    	col.createCard(card);
+  	})
+}
+
